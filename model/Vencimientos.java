224b40d1db17a521f521a223019fb481dda310d7
JulietaCaceres <julieta.agustina.caceres@gmail.com>
2017-07-15 14:21:13 -0300

    vencimientos

--- model/Vencimientos.java
+++ model/Vencimientos.java
@@ -1,5 +1,35 @@
-package FinancePartner.model;
+import java.util.ArrayList;
+import java.util.Date;
 
 
 public class Vencimientos {
+
+
+    protected ArrayList<Vencimiento> vencimientos;
+    protected Date fechaActual;
+
+    public void agregarVencimiento(Vencimiento unVencimiento){
+
+
+        vencimientos.add(unVencimiento);
+
+    }
+
+    public void mostrarVencimiento(){
+
+        System.out.println("Tus vencimientos:");
+
+        for (Vencimiento unVencimiento: vencimientos){
+                   procesarVencimiento(unVencimiento);
+        }
+    }
+
+    private void procesarVencimiento(Vencimiento unVencimiento) {
+
+        System.out.print("Servicio:");
+        System.out.println(unVencimiento.obtenerNombre());
+        System.out.println("Fecha de vencimiento:");
+        System.out.println(unVencimiento.obtenerFechaVencimiento());
+
+    }
 }
